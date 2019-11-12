ded9f86b6e07c157881bf3ad8a66f979e7a22479
Gilbert Velazquez <gv2793@gmail.com>
2019-11-11 18:48:55 -0700

    text addition

--- src/about.js
+++ src/about.js
@@ -5,7 +5,23 @@ const About = () => {
 	return (
 		<div className='about'>
 			<h3>My Story</h3>
-			<p>My name is Gilbert and I'm an ex-chemist who found my passion lies</p>
+			<p>My name is Gilbert and I'm an ex-chemist who is determined to become a self-taught Front End Developer.</p>
+			<br></br>
+			<p>Although a career in chemistry can be fulfilling, over my four year span in the field I came to realize
+			my interest had gradually begun to wane. Programming was something I was introduced to near the end of college in
+			2015 and had dabbled with one-and-off over the next few years. After going through tutorial after tutorial I 
+			finally told myself to get serious and began working on a series of small projects. As I worked on these I
+			all of the sudden understood what it meant to be totally interested and motivated by a subject, something I'd honestly
+			never felt about with chemistry. This is when I realized that I believe I've slowly, truly, become passionate
+			about programming and it was time to really try and start my career in the field.</p>
+			<br></br>
+			<p>After four years of working myself up, in August of 2019 I decided to leave my job as a chemist and focus
+			on a career in Front End Development. I'm ecstatic to continually add to this growing list of projects I hope
+			I can stay proud of in the future. I hope I can become another success story that people can look to when they
+			think they're stuck in a career they don't truly like and are afraid to move on; I hope I can motivate others
+			like others have for me in taking a plunge into something totally new and love every moment of it.</p>
+			<br></br>
+			<p>Thank you for reading my story - <span class='name'>G.V.</span></p>
 		</div>
 	)
 }
