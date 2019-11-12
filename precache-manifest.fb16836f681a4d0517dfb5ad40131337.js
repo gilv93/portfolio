5f2ad45fb418eb893ab32601e3d41a753a2b1bc6
Gilbert Velazquez <gv2793@gmail.com>
2019-11-11 18:20:40 -0700

    Updates

---
+++ precache-manifest.fb16836f681a4d0517dfb5ad40131337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dc1eca8a21e21cd000d5c12f1842c1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2f53b64406d9761606cc",
+    "url": "/portfolio/static/css/main.74e3d180.chunk.css"
+  },
+  {
+    "revision": "ff36dfebc68936fdf34c",
+    "url": "/portfolio/static/js/2.e45ea2c8.chunk.js"
+  },
+  {
+    "revision": "2f53b64406d9761606cc",
+    "url": "/portfolio/static/js/main.818898c8.chunk.js"
+  },
+  {
+    "revision": "333a05a02cb7ff62019d",
+    "url": "/portfolio/static/js/runtime-main.46d5065d.js"
+  }
+]);
