d37588cb38cc25ed32da00abf2d746a40140a2b6
Gilbert Velazquez <gv2793@gmail.com>
2019-11-11 19:17:32 -0700

    font size

--- src/about.js
+++ src/about.js
@@ -11,13 +11,13 @@ const About = () => {
 			my interest had gradually begun to wane. Programming was something I was introduced to near the end of college in
 			2015 and had dabbled with one-and-off over the next few years. After going through tutorial after tutorial I 
 			finally told myself to get serious and began working on a series of small projects. As I worked on these I
-			all of the sudden understood what it meant to be totally interested and motivated by a subject, something I'd honestly
-			never felt about with chemistry. This is when I realized that I believe I've slowly, truly, become passionate
+			realized I understood what it meant to be totally interested and motivated by a subject, something I'd
+			never felt about with chemistry. This is when I believe I had slowly, truly, become passionate
 			about programming and it was time to really try and start my career in the field.</p>
 			<br></br>
 			<p>After four years of working myself up, in August of 2019 I decided to leave my job as a chemist and focus
-			on a career in Front End Development. I'm ecstatic to continually add to this growing list of projects I hope
-			I can stay proud of in the future. I hope I can become another success story that people can look to when they
+			on a career in Front End Development. I'm ecstatic to continually add to this growing list of projects and look
+			back on them in the future. I hope I can become another success story that people can look to when they
 			think they're stuck in a career they don't truly like and are afraid to move on; I hope I can motivate others
 			like others have for me in taking a plunge into something totally new and love every moment of it.</p>
 			<br></br>
